78c22679eddfeffeda9880c6d2b2b0d9722e7317
abhinavgudu <abhinavguddu2000@gmail.com>
2026-03-25 00:55:38 +0530

    razorpay

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false as any,
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/:path*.html',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
